4882355a72d3090d3f014cac0206518d768ac559
Bryan Arroyo <arroyo_bryan@hotmail.com>
2017-04-26 04:24:19 -0500

    js docs

--- modelo.js
+++ modelo.js
@@ -1,3 +1,9 @@
+/** funcion P0 que es la probabilidad 
+        de nadie en el sistema
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var p0 = function(lambda, miu, m){
     var sumatoria = 0;
     var suma;
@@ -10,20 +16,50 @@ var p0 = function(lambda, miu, m){
     }
     return 1 / (sumatoria + ((1/rFact(m)) * Math.pow(lambda/miu, m) * ((m * miu) / ((m * miu) - lambda))));
 }
+
+ /** funcion L promedio de clientes en sistema
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var l = function(lambda, miu, m){
     var division;
     division = ((lambda * miu) * Math.pow(lambda/miu, m) ) / ((rFact(m - 1)) * Math.pow((m * miu)- lambda, 2));
     return (division * p0(lambda,miu,m)) + (lambda / miu);
 }
+
+
+ /** funcion W promedio de espera del cliente en el sistema
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var w = function(lambda, miu, m){
     return l(lambda, miu, m) / lambda;
 }
+   /** funcion Lq promedio clientes en cola
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var lq = function(lambda, miu, m){
     return l(lambda, miu, m) - (lambda / miu);
 }
+
+   /** funcion Wq promedio que pasan los clientes en cola
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var wq = function(lambda, miu, m){
     return w(lambda, miu, m) - (1/ miu);
 }
+
+ /** funcion ro probabilidad de que esten usando el servicio
+        @constructor 
+        @param {int} lambda - variable lambda num de llegadas
+        @param {int} miu - variable mui tiempo en atender
+        @param {int} m - numero de canales abiertos**/
 var ro = function(lambda, miu, m){
     return lambda / (miu * m);
 }
